bd6b7f86f340c86b6382f801a42ecc79f4c8b41c
Abhishek Ray <abhishek@nubewell.com>
2024-02-09 20:47:35 +0530

    inital commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button
+        className="btn btn-secondary"
+        onClick={() => {
+          console.log("product page ");
+        }}
+      >
+        CLICK ME
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
